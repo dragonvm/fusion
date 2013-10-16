3c1159f6d3abbae61b3bc3740f7051069d300771
Campus <Campus@192.168.3.121>
2013-10-16 13:43:18 +0200

    Creado proyecto

---
+++ AplicacionPruebaGit/src/aplicacionpruebagit/AplicacionPruebaGit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionpruebagit;
+
+/**
+ *
+ * @author Campus
+ */
+public class AplicacionPruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
